1147efe6ac060df7d8fd0dbab57ea79084ed9e88
Mohammadreza Yektamaram <mr.yekta@yahoo.com>
2019-09-30 20:46:55 +0330

    Init file

---
+++ switchLangKeys.js
@@ -0,0 +1,14 @@
+var
+  persianChars = ["ض", "ص", "ث", "ق", "ف", "غ", "ع", "ه", "خ", "ح", "ج", "چ", "ش", "س", "ی", "ب", "ل", "ا", "ت", "ن", "م", "ک", "گ", "ظ", "ط", "ز", "ر", "ذ", "د", "پ", "و", "؟"],
+  englishChars = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "z", "x", "c", "v", "b", "n", "m", ",", "?"],
+  switchTexts = function(text, find, replace) {
+    return text.replace(new RegExp("(" + find.map(function(i){return i.replace(/[.?*+^$[\]\\(){}|-]/g, "\\$&")}).join("|") + ")", "g"), function(s){ return replace[find.indexOf(s)]});
+  };;
+
+String.prototype.switchEnToFa = function() {
+  return switchTexts( this, englishChars, persianChars );
+};
+
+String.prototype.switchFaToEn = function() {
+  return switchTexts( this, persianChars, englishChars );
+};
